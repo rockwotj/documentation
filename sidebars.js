e848fe7a6f8a57f28594397b45ace763f9c30051
Edward Huang <edward@redpanda.com>
2023-08-01 19:58:04 -0700

    docs #1765 address review comments

--- sidebars.js
+++ sidebars.js
@@ -1442,21 +1442,8 @@
                     "labs/data-transform/rpk-transform-deploy",
                     "labs/data-transform/rpk-transform-list",
                     "labs/data-transform/rpk-transform-delete",
-                    {
-                      "type": "category",
-                      "label": "rpk transform registry",
-                      "link": {
-                        "type": "doc",
-                        "id": "labs/data-transform/rpk-transform-registry"
-                      },
-                      "items": [
-                        "labs/data-transform/rpk-transform-registry-describe",
-                        "labs/data-transform/rpk-transform-registry-list"
-                      ]
-                    }
                 ]
               },
-              "labs/data-transform/data-transform-sr-client",
             ]
         }
       ]
